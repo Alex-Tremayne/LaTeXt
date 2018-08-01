f0a3d31aa3586ed4956a1984fea6e5af26efed36
Alex Tremayne <alexjtremayne@gmail.com>
2018-08-01 10:27:45 +1000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='latext',
+      version='0.0.2',
+      description='For converting LaTeX to spoken text.',
+      url='https://github.com/Alex-Tremayne/LaTeXt',
+      author='Alex Tremayne',
+      author_email='alexjtremayne@gmail.com',
+      license='MIT',
+      classifiers=['Development Status :: 3 - Alpha',
+      'License :: OSI Approved :: MIT License',
+      'Programming Language :: Python :: 3.6'],
+      packages=['latext'],
+zip_safe=False)
